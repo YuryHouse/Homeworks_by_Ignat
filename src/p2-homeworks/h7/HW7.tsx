18f44de74f80a34ee9912fccc71792b4af5c3b33
YuryHouse <yury.house@yahoo.com>
2022-04-19 17:51:15 +0300

    HW7 added

--- src/p2-homeworks/h7/HW7.tsx
+++ src/p2-homeworks/h7/HW7.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import SuperSelect from './common/c5-SuperSelect/SuperSelect'
 import SuperRadio from './common/c6-SuperRadio/SuperRadio'
+import style from './HW7.module.css'
 
 const arr = ['x', 'y', 'z']
 
@@ -8,19 +9,19 @@ function HW7() {
     const [value, onChangeOption] = useState(arr[1])
 
     return (
-        <div>
+        <div className={style.background}>
             <hr/>
-            homeworks 7
+            <div className={style.textPosition}>homeworks 7</div>
 
             {/*should work (должно работать)*/}
-            <div>
+            <div className={style.selectPosition}>
                 <SuperSelect
                     options={arr}
                     value={value}
                     onChangeOption={onChangeOption}
                 />
             </div>
-            <div>
+            <div className={style.radioPosition}>
                 <SuperRadio
                     name={'radio'}
                     options={arr}
