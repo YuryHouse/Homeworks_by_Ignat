c723cdfaf0fc6db0d600195509173ad56eb90336
YuryHouse <yury.house@yahoo.com>
2022-04-27 10:55:25 +0300

    HW9 and CSS added

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import style from './HW9.module.css'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
@@ -7,28 +8,28 @@ function Clock() {
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = date?.toLocaleTimeString('en-GB') // fix with date
+    const stringDate = date?.toLocaleDateString('en-GB') // fix with date
 
     return (
-        <div>
+        <div className={style.hwButtons }>
             <div
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
--- src/p2-homeworks/h9/HW9.tsx
+++ src/p2-homeworks/h9/HW9.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Clock from './Clock'
+import style from './HW9.module.css'
 
 function HW9() {
     return (
-        <div>
+        <div className={style.hwStyle}>
             <hr/>
-            homeworks 9
+            <h4 className={style.hwTitle}>homeworks 9</h4>
 
             {/*should work (должно работать)*/}
             <Clock/>
