d3e09ba7704fbf2022ba26680c473314fc9eb5f0
YuryHouse <yury.house@yahoo.com>
2022-05-05 17:18:29 +0300

    HW10 added

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -1,35 +1,37 @@
 import React from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
-
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "./bll/store";
+import {loadingAC} from "./bll/loadingReducer";
+import style from './HW10.module.css'
 function HW10() {
     // useSelector, useDispatch
-    const loading = false
+    const loading = useSelector<AppStoreType>(state => state.loading.isLoading)
+    const dispatch = useDispatch()
+
 
     const setLoading = () => {
-        // dispatch
-        // setTimeout
-        console.log('loading...')
-    };
+        dispatch(loadingAC(true))
+        setTimeout(() => {
+            dispatch(loadingAC(false))
+        }, 2000)
+    }
 
     return (
-        <div>
+        <div className={style.hwStyle}>
             <hr/>
-            homeworks 10
-
-            {/*should work (должно работать)*/}
+            <h4 className={style.hwTitle}>homeworks 10</h4>
             {loading
                 ? (
-                    <div>крутилка...</div>
+                    <div className={style.preloader}>
+                        <div className={style.preloaderImage}></div>
+                    </div>
                 ) : (
                     <div>
                         <SuperButton onClick={setLoading}>set loading...</SuperButton>
                     </div>
                 )
             }
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<Alternative/>*/}
             <hr/>
         </div>
     )
--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,22 @@
 const initState = {
-
+ isLoading: false
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action:  loadingReducerType): typeof initState => { // fix any
     switch (action.type) {
-        case '': {
-            return state
+        case 'SHOW-LOADING': {
+            return {...state, isLoading: action.payload.isLoading}
         }
         default: return state
     }
 }
 
-export const loadingAC = (): any => {} // fix any
+type loadingReducerType = loadingACType
+
+type loadingACType = ReturnType<typeof loadingAC>
+export const loadingAC = (isLoading: boolean) => {
+    return {
+        type: 'SHOW-LOADING',
+        payload: {isLoading}
+    } as const
+}
