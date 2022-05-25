974faa632340163aac094fa8765b8e9a4d72a5e3
YuryHouse <yury.house@yahoo.com>
2022-05-25 17:17:34 +0300

    HW12 added

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,24 +1,47 @@
-import React from "react";
+import React, {ChangeEvent} from "react";
 import s from "./HW12.module.css";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "../h10/bll/store";
+import {changeThemeAC, StateType} from './bll/themeReducer'
+import SuperRadio from "../h7/common/c6-SuperRadio/SuperRadio";
+import SuperSelect from "../h7/common/c5-SuperSelect/SuperSelect";
 
 const themes = ['dark', 'red', 'some'];
 
-function HW12() {
-    const theme = 'some'; // useSelector
 
-    // useDispatch, onChangeCallback
+function HW12() {
+    const {theme, otherThemes} = useSelector<AppStoreType, StateType>(state => state.otherThemes)
+    const dispatch = useDispatch()
+    const onChangeCallback = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        dispatch(changeThemeAC(e.currentTarget.value))
+    }
 
     return (
-        <div className={s[theme]}>
-            <hr/>
-            <span className={s[theme + '-text']}>
-                homeworks 12
+        <div className={s.hwStyle}>
+            <div className={s[theme]}>
+                <hr/>
+                <h4 className={s.hwTitle}>homework 12</h4>
+                <span className={s[theme + '-text']}>
+                Choose Your Favorite Color:
             </span>
+                <div>
+                    <SuperSelect
+                        onChange={onChangeCallback}
+                        options={otherThemes}
+                        value={theme}
+                    />
+                </div>
+                <div>
+                    <SuperRadio
+                        onChange={onChangeCallback}
+                        name={'radio'}
+                        options={otherThemes}
+                        value={theme}
+                    />
+                </div>
 
-            {/*should work (должно работать)*/}
-            {/*SuperSelect or SuperRadio*/}
-
-            <hr/>
+                <hr/>
+            </div>
         </div>
     );
 }
--- src/p2-homeworks/h12/bll/themeReducer.ts
+++ src/p2-homeworks/h12/bll/themeReducer.ts
@@ -1,14 +1,31 @@
-const initState = {
+import {loadingAC} from "../../h10/bll/loadingReducer";
 
+const initState = {
+    theme: 'light',
+    otherThemes: ['light', 'dark', 'gold', 'green', 'red', 'violet']
 };
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export type StateType = {
+    theme: string;
+    otherThemes: string[]
+}
+
+export const themeReducer = (state = initState, action: themeReducerType): StateType => { // fix any
     switch (action.type) {
-        case "": {
-            return state;
+        case 'CHANGE-THEME': {
+            return {...state, theme: action.payload.theme}
         }
-        default: return state;
+        default:
+            return state;
     }
 };
 
-export const changeThemeC = (): any => {}; // fix any
+type themeReducerType = changeThemeACType
+
+type changeThemeACType = ReturnType<typeof changeThemeAC>
+export const changeThemeAC = (theme: string) => {
+    return {
+        type: 'CHANGE-THEME',
+        payload: {theme}
+    } as const
+}
